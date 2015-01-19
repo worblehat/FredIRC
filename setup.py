129b71c6cde67cef926f044039d7469d6f1099a5
Tobias Marquardt <tm@tobix.eu>
2015-01-20 00:56:02 +0100

    Additional Category in setup.py.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Communications :: Chat :: Internet Relay Chat',
